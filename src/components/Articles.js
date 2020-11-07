e827ddbf8ddf5e971b6eca1693fec6b83f31d9b3
erikgonzalez <erik.gonzalez.ac@gmail.com>
2020-11-07 14:14:16 -0700

    Mostrar listado de articulos.

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,7 +1,15 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import Moment from 'react-moment'
+import 'moment/locale/es';
+import Global from '../Global';
+import default_img from '../assets/images/default_img.png';
+
+
 
 class Articles extends Component {
+
+    url = Global.url;
 
     state = {
         articles: [],
@@ -15,23 +23,49 @@ class Articles extends Component {
     }
 
     getArticles = ()=>{
-        axios.get("http://localhost:3900/api/articles").then(res =>{
+        axios.get(this.url+"articles").then(res =>{
             this.setState({
                 articles: res.data.articles,
                 status: 'success'
             });
-            console.log(this.state);
+             //console.log(this.state);
         });
     }
 
     render(){
 
         if(this.state.articles.length >= 1){
+
+            var listArticles = this.state.articles.map((article)=>{
+                return (
+                    <article className="article-item" id="article-template">
+                    <div className="image-wrap">
+                        {article.image !== null ? (
+                             <img src={this.url+'get-image/'+article.image}
+                             alt={article.title} />
+                        ): (
+                            <img src={default_img}
+                            alt="imagen por defecto" />
+                        )
+                        
+                        }
+
+                       
+                    </div>
+                <h2>{article.title}</h2>
+                    <span className="date">
+                       <Moment locale="es" fromNow date={article.date}>  </Moment>
+                    </span>
+                    <a href="article.html">Leer más</a>
+                    <div className="clearfix"></div>
+                </article>
+                );
+            })
+
+
             return(
                 <div id="articles">
-                    <h1>
-                        Listado de articulos
-                    </h1>
+                   {listArticles}
                 </div>
             );
         }else if(this.state.articles.length === 0 && this.state.status === 'success'){
